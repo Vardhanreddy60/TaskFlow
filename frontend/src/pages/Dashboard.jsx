5f46192eaaba2b1da00b25ea503660c87e31991f
Vardhanreddy60 <vardhanreddy6061@gmail.com>
2025-12-10 21:46:32 +0530

    Connected frontend to backend live URL

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -12,7 +12,7 @@ import {
 } from '../assets/dummy'
 
 // API Base
-const API_BASE = "http://localhost:4000/api/tasks"
+const API_BASE = "http://://taskflow-gc5e.onrender.com"
 
 const Dashboard = () => {
   const { tasks, refreshTasks } = useOutletContext()
